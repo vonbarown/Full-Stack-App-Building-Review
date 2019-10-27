7ef8e05c0d23d95b7aef85791f5845859a8d3f39
vonbarown <vonielbrown@pursuit.org>
2019-10-27 10:16:42 -0400

    adding item file

---
+++ solution/server/models/Item.js
@@ -0,0 +1,9 @@
+class Item {
+    constructor(name, units, arrived_at, type, price) {
+        this.name = name;
+        this.units = units;
+        this.arrived_at = arrived_at;
+        this.type = type;
+        this.price = price
+    }
+}
