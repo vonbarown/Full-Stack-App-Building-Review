c7684cec7e0c1c11926f9ff5cd3a89bb948a43ec
vonbarown <vonielbrown@pursuit.org>
2019-10-27 20:03:20 -0400

    adding a front end

--- solution/server/app.js
+++ solution/server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const cors = require('cors')
 
 const app = express();
 const port = 3000;
@@ -10,6 +11,10 @@ app.use(
         extended: true,
     })
 );
+
+app.use(express.json());
+
+app.use(cors());
 
 const vegetableRouter = require('./routes/vegetablesRouter')
 
